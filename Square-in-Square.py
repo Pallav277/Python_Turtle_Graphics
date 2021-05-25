42864e6d267ce7843c1bcd4abba574c1a48d81b5
Pallav277 <58349039+Pallav277@users.noreply.github.com>
2021-05-26 00:26:44 +0530

    Create Square-in-Square.py

---
+++ Square-in-Square.py
@@ -0,0 +1,94 @@
+import turtle
+wn = turtle.getscreen()
+wn.bgcolor("black")
+s = turtle.Turtle()
+
+for i in range(4):
+    s.begin_fill()
+    s.forward(100)
+    s.left(90)
+    s.pencolor("red")
+    s.forward(100)
+    s.fillcolor("red")
+    s.end_fill()
+
+for i in range(4):
+    s.begin_fill()
+    s.forward(90)
+    s.left(90)
+    s.pencolor("green")
+    s.forward(90)
+    s.fillcolor("green")
+    s.end_fill()
+
+for i in range(4):
+    s.begin_fill()
+    s.forward(80)
+    s.left(90)
+    s.pencolor("yellow")
+    s.forward(80)
+    s.fillcolor("yellow")
+    s.end_fill()
+
+for i in range(4):
+    s.begin_fill()
+    s.forward(70)
+    s.left(90)
+    s.pencolor('brown')
+    s.forward(70)
+    s.fillcolor("brown")
+    s.end_fill()
+
+for i in range(4):
+    s.begin_fill()
+    s.forward(60)
+    s.left(90)
+    s.pencolor("white")
+    s.forward(60)
+    s.fillcolor("white")
+    s.end_fill()
+
+for i in range(4):
+    s.begin_fill()
+    s.forward(50)
+    s.left(90)
+    s.pencolor("blue")
+    s.forward(50)
+    s.fillcolor("blue")
+    s.end_fill()
+
+for i in range(4):
+    s.begin_fill()
+    s.forward(40)
+    s.left(90)
+    s.pencolor("pink")
+    s.forward(40)
+    s.fillcolor("pink")
+    s.end_fill()
+
+for i in range(4):
+    s.begin_fill()
+    s.forward(30)
+    s.left(90)
+    s.pencolor("cyan")
+    s.forward(30)
+    s.fillcolor("cyan")
+    s.end_fill()
+
+for i in range(4):
+    s.begin_fill()
+    s.forward(20)
+    s.left(90)
+    s.pencolor('purple')
+    s.forward(20)
+    s.fillcolor("purple")
+    s.end_fill()
+
+for i in range(4):
+    s.begin_fill()
+    s.forward(10)
+    s.left(90)
+    s.pencolor("lime")
+    s.forward(10)
+    s.fillcolor("lime")
+    s.end_fill()
